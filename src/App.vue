b18132f888087ae94d6b3d2604b5dad56e5b32e3
yasyars <yasya.rusyda@gmail.com>
2020-09-04 03:45:16 +0700

    add unfinished navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar">
+      <router-link class= "nav-item" to="/" >Beranda</router-link>
+      <router-link class= "nav-item" to="/about">Tentang</router-link>
+      <router-link class= "nav-item" to="/kegiatan">Kegiatan</router-link>
+    </nav>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
+.nav-item{
+  align-items: center;
+  padding: 10px;
+  font-weight: bold;
+  color: #2c3e50;
+  font-style: normal;
+  text-decoration: none;
+}
+.navbar{
+  background-color: white;
+  box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.1);
+  position: absolute;
+  width: 1366px;
+  /* height: 75px; */
+  /* left: 100px;
+  top: 0px; */
+  padding: 30px;
+  align-items: center;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+/* #nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+} */
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/Kegiatan.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Kegiatan"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Kegiatan',
+  components: {
+    HelloWorld
+  }
+}
+</script>
