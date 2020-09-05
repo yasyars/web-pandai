472dcdf641ce38ff3c0612eb292b46fca2ab5286
NiraRamadhani <13516018@std.stei.itb.ac.id>
2020-09-06 01:29:51 +0700

    add homepage desc

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -34,7 +34,7 @@
             Bebras Indonesia
         </h2>
         <p style="text-align: justify; margin-top: 15px">
-          Bebras pertama kali digelar di Lithuania (www.bebras.org), merupakan aktivitas ekstra kurikuler yang mengedukasi kemampuan problem solving dalam informatika dengan jumlah peserta terbanyak di dunia. Siswa peserta akan mengikuti kompetisi bebras di bawah supervisi guru, yang dapat mengintegrasikan tantangan tersebut dalam aktivitas mengajar guru. Kompetisi ini dilakukan setiap tahun secara online melalui komputer.
+          Bebras pertama kali digelar di Lithuania (www.bebras.org), merupakan aktivitas ekstra kurikuler yang mengedukasi kemampuan problem solving dalam informatika dengan jumlah peserta terbanyak di dunia. Siswa peserta akan mengikuti kompetisi bebras di bawah supervisi guru, yang dapat mengintegrasikan tantangan tersebut dalam aktivitas mengajar guru. Kompetisi ini dilakukan setiap tahun secara online melalui komputer. Yang dilombakan dalam kompetisi adalah sekumpulan soal yang disebut Bebras task. Bebras task disajikan dalam bentuk uraian persoalan yang dilengkapi dengan gambar yang menarik, sehingga siswa dapat lebih mudah memahami soal. Soal-soal tersebut dapat dijawab tanpa perlu belajar informatika terlebih dahulu, tapi soal tersebut sebetulnya terkait pada konsep tertentu dalam informatika dan computational thinking. Bebras task sekaligus menunjukkan aspek informatika dan computational thinking.
         </p>
       </v-col>
       <v-col
@@ -45,7 +45,7 @@
             Gerakan PANDAI
         </h2>
         <p style="text-align: justify; margin-top: 15px">
-          Gerakan PANDAI merupakan kegiatan yang dilaksanakan oleh Bebras Indonesia dengan dukungan Google.org untuk menebarkan dan mengajarkan penguasaan computational thinking kepada siswa-siswa melalui guru-guru berbagai mata pelajaran.
+          Gerakan PANDAI merupakan program yang diselenggarakan oleh Bebras Indonesia dengan disponsori oleh Google.org dan mendapat dukungan dari Direktorat Jenderal Guru dan Tenaga Kependidikan & Direktorat Jenderal Pendidikan Tinggi Kementerian Pendidikan dan Kebudayaan. Tujuan dari gerakan ini adalah memberikan pelatihan Computational Thinking (CT)kepada guru, dengan harapan agar keterampilan yang didapatkan tersebut dapat meningkatkan keterampilan berfikir secara kritis dan kreatif para siswa serta memberikan fondasi untuk menyiapkan generasi penerus yang berdaya saing di era Revolusi Industri 4 saat ini. Bebras Indonesia sebagai pelaksana Gerakan PANDAI adalah himpunan relawan dosen Perguruan Tinggi di berbagai kota di Indonesia yang turut serta dalam sebuah inisiatif internasional yang bertujuan untuk mempromosikan CT (berpikir dengan landasan komputasi atau informatika), di kalangan guru dan murid mulai tingkat SD, SMP, SMA, serta masyarakat luas.
         </p>
       </v-col>
       <!-- <v-col
